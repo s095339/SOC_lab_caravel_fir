ce692849b0f7d55ae445fd46d6d26343c7e7252a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-11-07 01:31:50 -0500

    [modify]wb_axilite.v

--- rtl/user/WB_AXILITE.v
+++ rtl/user/WB_AXILITE.v
@@ -15,26 +15,53 @@ module WB_AXILITE
     input wbs_stb_i,
     input wbs_cyc_i,
     input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    input wire [3:0] wbs_sel_i,
+    input wire [31:0] wbs_dat_i,
+    input wire [31:0] wbs_adr_i,
+    output reg wbs_ack_o,
+    output reg [31:0] wbs_dat_o,
     //axilite ports=================================
     //write(input)--
     input    wire                     awready,
+    output   reg                     awvalid,
+    output   reg [(pADDR_WIDTH-1):0] awaddr,
     input    wire                     wready,
-    output   wire                     awvalid,
-    output   wire [(pADDR_WIDTH-1):0] awaddr,
-    output   wire                     wvalid,
-    output   wire [(pDATA_WIDTH-1):0] wdata,
+    output   reg                     wvalid,
+    output   reg [(pDATA_WIDTH-1):0] wdata,
     //read(output)---
     input    wire                     arready,
-    output   wire                     rready,
-    output   wire                     arvalid,
-    output   wire [(pADDR_WIDTH-1):0] araddr,
+    output   reg                     arvalid,
+    output   reg [(pADDR_WIDTH-1):0] araddr,
+    output   reg                     rready,
     input    wire                     rvalid,
     input    wire [(pDATA_WIDTH-1):0] rdata
 );
+
+always @(*) begin
+    case (wbs_we_i)
+        1'b0 :begin
+            awvalid = 1'b0;
+            wvalid = 1'b0;
+            awaddr = 32'd0;
+            wdata = 32'd0;
+            wbs_ack_o = rready;
+            wbs_dat_o = rdata;
+            arvalid = wbs_stb_i;
+            araddr = wbs_adr_i[7:0];
+            rready = wbs_stb_i;
+        end
+        1'b1 :begin
+            awvalid = wbs_stb_i;
+            wvalid = wbs_stb_i;
+            awaddr = wbs_adr_i[7:0];
+            wdata = wbs_dat_i;
+            wbs_ack_o = wready;
+            wbs_dat_o = 32'd0;
+            arvalid = 1'b0;
+            araddr = 32'd0;
+            rready = 1'b0;
+        end
+    endcase
+end
 
 endmodule
